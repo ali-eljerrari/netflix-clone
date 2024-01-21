0b1a0dca627697d381b9cffc8e8eb9375f96cac6
Coding_Dynasty <eljerrariali8@gmail.com>
2024-01-21 16:09:03 +0100

    ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,65 @@
-import { useEffect, useState } from "react";
-import Axios from "./config/Axios";
+import { StarIcon } from "@chakra-ui/icons";
 import {
+  Button,
   Card,
   CardBody,
-  Image,
-  Stack,
-  Heading,
-  Text,
   CardFooter,
+  Heading,
+  Image,
   Input,
-  Button,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
-import { StarIcon } from "@chakra-ui/icons";
+import { useEffect, useState } from "react";
+import Axios from "./config/Axios";
 
 import ReactPaginate from "react-paginate";
 import useStore from "./store/store.js";
 
+import { Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+import { Container, keyframes } from "@chakra-ui/react";
+
 function App() {
   const { movies, setMovies, page, term } = useStore();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      await Axios.get(`/search/movie?query=${term}&page=${page}`)
-        .then(({ data }) => {
-          setMovies(data);
-        })
-        .catch((err) => console.error(err));
+      try {
+        const response = await Axios.get(
+          `/search/movie?query=${term}&page=${page}`
+        );
+
+        if (response.status === 200) {
+          setMovies(response.data);
+        }
+      } finally {
+        setLoading(false);
+      }
     };
 
     if (term) {
       fetchData();
     }
-  }, [page, setMovies, term]);
+  }, [loading, page, setMovies, term]);
 
   return (
-    <div className="flex items-center  justify-center p-4">
-      <PaginatedItems itemsPerPage={movies.page} />
+    <div className="flex items-center justify-center p-4">
+      <PaginatedItems
+        itemsPerPage={movies.page}
+        loading={loading}
+        setLoading={setLoading}
+      />
     </div>
   );
 }
 
 export default App;
 
-function PaginatedItems() {
+// eslint-disable-next-line react/prop-types
+function PaginatedItems({ loading, setLoading }) {
   const { movies, setpage, setTerm } = useStore();
   const [input, setInput] = useState("");
 
@@ -50,16 +67,30 @@ function PaginatedItems() {
 
   const handlePageClick = (event) => {
     setpage(event.selected + 1);
+    setLoading(true);
   };
 
+  const animationKeyframes = keyframes`
+  0% { transform: scale(1) rotate(0); border-radius: 20%; }
+  25% { transform: scale(2) rotate(0); border-radius: 20%; }
+  50% { transform: scale(2) rotate(270deg); border-radius: 50%; }
+  75% { transform: scale(1) rotate(270deg); border-radius: 50%; }
+  100% { transform: scale(1) rotate(0); border-radius: 20%; }
+`;
+
+  const animation = `${animationKeyframes} 400ms ease-in-out infinite`;
+
   return (
-    <div className="flex flex-col items-center jus w-full lg:w-8/12 m-4">
+    <div className="flex flex-col items-center jus w-full lg:w-8/12 m-4 max-w-7xl">
       <form
-        className="flex flex-col w-full"
+        className={`flex flex-col w-full ${loading ? "mb-40" : ""}`}
         onSubmit={(e) => {
           e.preventDefault();
-          setTerm(input);
-          setInput("");
+          if (input) {
+            setTerm(input);
+            setInput("");
+            setLoading(true);
+          }
         }}
       >
         <Input
@@ -69,21 +100,42 @@ function PaginatedItems() {
           size="sm"
           className="text-center mb-2"
         />
-        <Button type="submit" colorScheme="blue">
+        <Button type="submit" style={{ background: "#FF0080", color: "#fff" }}>
           Search
         </Button>
       </form>
-      <Items />
-      <ReactPaginate
-        className="pagination"
-        breakLabel="..."
-        nextLabel=">>"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="<<"
-        renderOnZeroPageCount={null}
-      />
+      {loading ? (
+        <Container
+          h="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Box
+            as={motion.div}
+            animation={animation}
+            padding="2"
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+            width="12"
+            height="12"
+            display="flex"
+          />
+        </Container>
+      ) : (
+        <>
+          <Items />
+          <ReactPaginate
+            className="pagination"
+            breakLabel="..."
+            nextLabel=">>"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel="<<"
+            renderOnZeroPageCount={null}
+          />
+        </>
+      )}
     </div>
   );
 }
